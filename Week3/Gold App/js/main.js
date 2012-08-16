4f43f5609c67723022e9eac493432c95be66f1ac
Ryan Trease <rstrease@fullsail.edu>
2012-08-16 18:17:09 -0500

    fixed code to validate form

--- Week3/Gold App/js/main.js
+++ Week3/Gold App/js/main.js
@@ -1,14 +1,46 @@
-var parseToolForm = function(data){
-	console.log(data);
-};
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#additem').on('pageinit', function(){
 
-$(document).ready(function(){
-	var tform = $('#toolForm');
-	tform.validate({
-		invalidHandler: function(form, validator){},
-		submitHandler: function(){
-			var data = tform.serializeArray();
-			parseToolForm(data);
+		var myForm = $('#toolForm');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
 		}
 	});
+	
+	$('#reset').click(function() {
+	    $('#qty').val('1').slider('refresh');
+	    $('#selectType').val('selection').selectmenu('refresh');
+		$('#type-2').attr('checked',false).checkboxradio('refresh');
+		$('#type-1').attr('checked',true).checkboxradio('refresh');
+	});
+	
 });
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
