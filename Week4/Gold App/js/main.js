96d4c266b0d2e0f927e5e103c3e53c0b81ed5af9
Ryan Trease <rstrease@fullsail.edu>
2012-08-20 21:13:38 -0500

    updated storeData unction

--- Week4/Gold App/js/main.js
+++ Week4/Gold App/js/main.js
@@ -26,7 +26,7 @@ $('#additem').on('pageinit', function(){
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 var autofillData = function (){
-	 
+
 };
 
 var getData = function(){
@@ -34,13 +34,25 @@ var getData = function(){
 };
 
 var storeData = function(data){
-	
+	localStorage.name = $('#name').val();
+	localStorage.selectType = $('#selectType').val();
+	localStorage.make = $('#make').val();
+	localStorage.mnumber = $('#mnumber').val();
+	localStorage.snumber = $('#snumber').val();
+	localStorage.dpurchased = $('#dpurchased').val();
+	localStorage.wpurchased = $('#wpurchased').val();
+	localStorage.price = $('#price').val();
+	localStorage.ev = $('#ev').val();
+	localStorage.condition = $('input[name=condition]:checked').val();
+	localStorage.qty = $('#qty').val();
+	localStorage.dateadded = $('#dateadded').val();
+	localStorage.notes = $('#notes').val();
 }; 
 
 var	deleteItem = function (){
-			
+		
 };
 					
 var clearLocal = function(){
-
+	
 };
