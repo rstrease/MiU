76f904b7334c95ef5b245196302588e394ae5598
Ryan Trease <rstrease@gmail.com>
2012-08-22 16:47:35 -0500

    added clearLocal function and updated the storeData function

--- Week4/Gold App/js/main.js
+++ Week4/Gold App/js/main.js
@@ -1,15 +1,16 @@
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
-		
+});
+
 $('#additem').on('pageinit', function(){
 		delete $.validator.methods.date;
 		var myForm = $('#toolForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
+			alert("Please fill in the following missing fields: ");
 			},
 			submitHandler: function() {
-		var data = myForm.serializeArray();
+		var data = $(".myForm").serializeArray();
 			storeData(data);
 		}
 	});
@@ -21,6 +22,11 @@ $('#additem').on('pageinit', function(){
 		$('#type-1').attr('checked',true).checkboxradio('refresh');
 	});
 	
+	$("#clear").click(function(){
+        clearLocal();
+        return false;
+    });
+ 
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
@@ -30,29 +36,50 @@ var autofillData = function (){
 };
 
 var getData = function(){
-
+	
 };
 
 var storeData = function(data){
-	localStorage.name = $('#name').val();
-	localStorage.selectType = $('#selectType').val();
-	localStorage.make = $('#make').val();
-	localStorage.mnumber = $('#mnumber').val();
-	localStorage.snumber = $('#snumber').val();
-	localStorage.dpurchased = $('#dpurchased').val();
-	localStorage.wpurchased = $('#wpurchased').val();
-	localStorage.price = $('#price').val();
-	localStorage.ev = $('#ev').val();
-	localStorage.condition = $('input[name=condition]:checked').val();
-	localStorage.qty = $('#qty').val();
-	localStorage.dateadded = $('#dateadded').val();
-	localStorage.notes = $('#notes').val();
+	var key;
+	if(!key){
+    	var id = Math.floor(Math.random()*100000001);
+    }
+    else{
+    	id = key;
+    }
+
+    var item = {};
+		item.name = ["Name:", $('#name').val()];
+        item.selectType = ["Tool/Item Type:", $('#selectType').val()];
+        item.make = ["Make:", $('#make').val()];
+        item.mnumber = ["Model Number:", $('#mnumber').val()];
+        item.snumber = ["Serial Number:", $('#snumber').val()];
+        item.dpurchased = ["Date Purchased:", $('#dpurchased').val()];
+        item.wpurchased = ["Where Purchased:", $('#wpurchased').val()];
+        item.price = ["Price:", $('#price').val()];
+        item.ev = ["Estimated Value:", $('#ev').val()];
+        item.condition = ["Purchase Type:", $('input[name=condition]:checked').val()];
+        item.qty = ["Quantity:", $('#qty').val()];
+        item.dateadded = ["Date Added:", $('#dateadded').val()];
+        item.notes = ["Additional notes:", $('#notes').val()];
+		
+		//save to local storage: use stringify to convert to string
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Your item was stored successfully!");
 }; 
+
+var clearLocal = function(){
+	if (localStorage.length === 0){
+    		alert("You have no data to clear.");
+			$.mobile.changePage('#additem', { transition: "slide"});			
+    	}
+    	else{
+    		localStorage.clear();
+    		alert("Your contents have been deleted.");
+			$.mobile.changePage('#additem', { transition: "slide"});
+    	}
+	};
 
 var	deleteItem = function (){
 		
-};
-					
-var clearLocal = function(){
-	
 };
